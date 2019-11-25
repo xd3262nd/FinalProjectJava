884009234801724516dd3834cb67c94b3c9a0c2f
Hui Qian Tan <xd3262nd@go.minnstate.edu>
2019-11-25 13:51:59 -0600

    --Finish up the layout of my apps

---
+++ src/main/java/ToDoListManager.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class ToDoListManager {
+    private JPanel mainPanel;
+    private JLabel titleLabel;
+    private JTextField tasksNameText;
+    private JTextField descriptionText;
+    private JComboBox priorityComboBox;
+    private JComboBox categoryComboBox;
+    private JButton addButton;
+    private JPanel addTasksPanel;
+    private JLabel tasksNameLabel;
+    private JLabel descriptionLabel;
+    private JLabel priorityLabel;
+    private JLabel categoryLabel;
+    private JList incompleList;
+    private JPanel toDoListPanel;
+    private JButton completedTaksButton;
+    private JButton showAllTasks;
+    private JList CompletedList;
+    private JLabel toDoLabel;
+    private JLabel completedList;
+    private JComboBox searchByPriorityComboBox;
+    private JButton searchByPriority;
+    private JButton searchByCategoryButton;
+    private JPanel searchPanel;
+    private JComboBox searchList;
+    private JList list1;
+    private JLabel searchListDescription;
+    private JButton quitButton;
+    private JPanel controlPanel;
+    private JButton editButton;
+}
